c9ee1aa1a9b019fcd7daab1180e19cd814481077
omit-krish-demo <muralikrishna1062001@gmail.com>
2025-11-25 18:57:47 +0530

    first commit

---
+++ src/main/java/com/omitkrish/thymleaf/SpringDataAuditAppApplication.java
@@ -0,0 +1,13 @@
+package com.omitkrish.thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAuditAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAuditAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omitkrish/thymleaf/SpringDataAuditAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omitkrish.thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAuditAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
